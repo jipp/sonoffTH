c28c908b288ede28c9d245d582b5a1979eeaf8d0
jipp <wolfgang.keller@wobilix.de>
2016-12-21 08:58:52 +0100

    added more output

--- extra_script.py
+++ extra_script.py
@@ -4,17 +4,20 @@ from shutil import copyfile
 import os
 
 
-def after_bin(target, source, env):
-    dir = "bin\\"
-    src = str(source[0])
-    dst = env.get('CPPDEFINES')[9][1].strip('\\"') + ".bin"
+def after_bin(source, target, env):
+    print "source: " + str(source[0])
+    print "target: " + str(target[0])
 
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    path = "bin"
+    srcFile = str(target[0])
+    dstFile = os.path.join(path, env.get('CPPDEFINES')[9][1].strip('\\"') + ".bin")
 
-    print "src: " + src
-    print "dst: " + dir + dst
-    copyfile(src, dir + dst)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    print "src: " + srcFile
+    print "dst: " + dstFile
+    copyfile(srcFile, dstFile)
 
 
 print "Current build targets", map(str, BUILD_TARGETS)
