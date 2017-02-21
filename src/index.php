a8baa40cb71f3cd959e273dc3c8548324035e708
jipp <wolfgang.keller@wobilix.de>
2017-02-21 21:29:10 +0100

    show log and mac table

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>OTA ESP</title>
+</head>
+<body>
+<?php
+	require_once('sqlite3.php');
+	$sql = new DB('arduino.db');
+	$sql->create('arduino.db');
+    echo "<h1>esp table:</h1>\n";
+	$sql->selectAll('esp');
+    echo "<h1>log table:</h1>\n";
+    $sql->selectAll('log');
+?> 
+</body>
+</html>
